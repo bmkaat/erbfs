625170b64a37e1f4b6d8558074f08d6d98a2eaf7
bmkaat <bmkaat@gmail.com>
2022-07-15 19:57:23 +0300

    the real one

--- erbfs.c
+++ erbfs.c
@@ -1,60 +1,80 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 int main()
 {
-int i,j,k;
-printf("number of rows: ");
-scanf("%i", &i);
-printf("number of columns: ");
-scanf("%i", &j);
-printf("number of elements: ");
-scanf("%i", &k);
-int game[i][j][k];
-for (int i1=0; i1<i; i1++)
-{
-    for(int j1=0; j1<j; j1++)
-    {   
-        printf("enter elements of [%i][%i]", i1+1, j1+1);
-        for (int k1=0; k1<k; k1++)
-        {
-            scanf("%d", &game[i1][j1][k1]);
-        }
-    }
-}
-/*for (int i1=0; i1<i; i1++)
-{
-    for(int j1=0; j1<j; j1++)
-    {   
-        printf("number of elements in [%i][%i]: ", i1+1,j1+1);
-        scanf("%i", &k);
-        int element[k];
-        game[i1][j1]= element[k];
-        printf("enter elements of [%i][%i]", i1+1,j1+1);
-        for (int k1=0; k1<k; k1++)
-        {
-            int k2;
-            scanf("%i", &k2);
-            element[k1]=k2;
-        }
-    }
-}
-
-for (int i1=0; i1<i; i1++)
-{
-    printf("(");
-    for(int j1=0; j1<j; j1++)
+    int i,j,k;
+    printf("number of rows: ");
+    scanf("%i", &i);
+    printf("number of columns: ");
+    scanf("%i", &j);
+    k=j;
+    int game[i][j][k];
+    memset(game, 0, sizeof game);
+    for (int i1=0; i1<i; i1++)
     {
-        printf("[");
-        for(int k1=0; k1<k; k1++)
-        {
-            printf("%d ", game[i1][j1][k1]);
+        for(int j1=0; j1<j; j1++)
+        {   
+            printf("number of elements in [%i][%i]: ", i1+1, j1+1);
+            int k1;
+            scanf("%i", &k1);
+            for (int k2=0; k2<k1; k2++)
+            {
+                printf("enter elements: ");
+                int k3;
+                scanf("%d", &k3);
+                k3--;
+                game[i1][j1][k3]=1;
+            }
         }
-        printf("] ");
     }
-    printf(")\n");
-}
-*/
+    int g,r;
+    printf("how many green hints? ");
+    scanf("%i", &g);
+    printf("how many red hints? ");
+    scanf("%i", &r);
+    for(int g1=0; g1<g; g1++)
+    {
+        int g2,e2;
+        printf("what row? ");
+        scanf("%i", &g2);
+        printf("what element? ");
+        scanf("%i", &e2);
+    }
 
-printf("%d", game[1][0][1]);
+    /*
+    for (int i1=0; i1<i; i1++)
+    {
+        for(int j1=0; j1<j; j1++)
+        {   
+            printf("number of elements in [%i][%i]: ", i1+1,j1+1);
+            scanf("%i", &k);
+            int element[k];
+            game[i1][j1]= element[k];
+            printf("enter elements of [%i][%i]", i1+1,j1+1);
+            for (int k1=0; k1<k; k1++)
+            {
+                int k2;
+                scanf("%i", &k2);
+                element[k1]=k2;
+            }
+        }
+    }
+
+    for (int i1=0; i1<i; i1++)
+    {
+        printf("(");
+        for(int j1=0; j1<j; j1++)
+        {
+            printf("[");
+            for(int k1=0; k1<k; k1++)
+            {
+                printf("%d", game[i1][j1][k1]);
+            }
+            printf("]");
+        }
+        printf(")\n");
+    }
+    */
     return 0;
 }
